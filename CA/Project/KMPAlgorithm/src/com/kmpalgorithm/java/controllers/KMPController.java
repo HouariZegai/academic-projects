2b406d89c266694c33bb84518691656ed076cba6
HouariZEGAI <houarizegai14@gmail.com>
2018-11-11 23:16:40 +0100

    Adding Search Engine KMPAlgorithms Project - CA

--- CA/Project/KMPAlgorithm/src/com/kmpalgorithm/java/controllers/KMPController.java
+++ CA/Project/KMPAlgorithm/src/com/kmpalgorithm/java/controllers/KMPController.java
@@ -1,14 +1,17 @@
 package com.kmpalgorithm.java.controllers;
 
-import com.jfoenix.controls.JFXSpinner;
-import com.jfoenix.controls.JFXTextArea;
-import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.controls.JFXToggleButton;
+import com.jfoenix.controls.*;
 import com.kmpalgorithm.java.Launcher;
+import com.kmpalgorithm.java.searchengine.KMP;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
 
@@ -17,10 +20,13 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class KMPController implements Initializable {
 
+    @FXML
+    private VBox root;
     @FXML
     private JFXTextArea areaInput;
     @FXML
@@ -32,13 +38,17 @@ public class KMPController implements Initializable {
     @FXML
     private HBox boxResult;
     @FXML
-    private Hyperlink linkTimes;
-    @FXML
     private TextFlow fieldResult;
+    // Toast Error Msg
+    private JFXSnackbar toastMsg;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        toastMsg = new JFXSnackbar(root);
 
+        areaInput.setOnKeyTyped(e -> {onRemoveOutput();});
+        fieldPattern.setOnKeyTyped(e -> {onRemoveOutput();});
+        toggleCaseSensitive.setOnAction(e -> onRemoveOutput());
     }
 
     @FXML
@@ -71,12 +81,48 @@ public class KMPController implements Initializable {
 
     @FXML
     private void onSearch() {
+        if(areaInput.getText() == null || areaInput.getText().trim().isEmpty()) {
+            toastMsg.show("Input is Empty !", 2000);
+            return;
+        }
+        if(fieldPattern.getText() == null || fieldPattern.getText().trim().isEmpty()) {
+            toastMsg.show("Pattern is Empty !", 2000);
+            return;
+        }
 
+        KMP kmp = new KMP(areaInput.getText(), toggleCaseSensitive.isSelected());
+        List<Integer> foundIndex = kmp.searchAndGetIndex(fieldPattern.getText());
+
+        Label lblFound = (Label) boxResult.getChildren().get(0);
+        MaterialDesignIconView iconEmojyFound = (MaterialDesignIconView) lblFound.getGraphic();
+        Hyperlink linkCountNumber = (Hyperlink) boxResult.getChildren().get(1);
+
+        if(!foundIndex.isEmpty()) {
+            lblFound.setText("Found !");
+            lblFound.setTextFill(Paint.valueOf("#00b248"));
+            iconEmojyFound.setGlyphName("EMOTICON_HAPPY");
+            iconEmojyFound.setFill(Paint.valueOf("#00b248"));
+            linkCountNumber.setText(foundIndex.size() + " Times");
+        } else {
+            lblFound.setText("Not Found !");
+            lblFound.setTextFill(Paint.valueOf("#000"));
+            iconEmojyFound.setGlyphName("EMOTICON_SAD");
+            iconEmojyFound.setFill(Paint.valueOf("#000"));
+            linkCountNumber.setText("0 Times");
+            linkCountNumber.setDisable(true);
+        }
+        boxResult.setVisible(true);
     }
 
     @FXML
     private void onLinkTimes() {
 
+    }
+
+    private void onRemoveOutput() { // remove output
+        boxResult.setVisible(false);
+        ((Hyperlink) boxResult.getChildren().get(1)).setDisable(false);
+        fieldResult.getChildren().clear();
     }
 
 }
