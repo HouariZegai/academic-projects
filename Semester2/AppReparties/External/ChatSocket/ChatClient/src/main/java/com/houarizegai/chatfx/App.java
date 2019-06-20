b1f66c4a2b943e8f12050a697bc54d818e32f9d2
HouariZegai <houarizegai14@gmail.com>
2019-06-20 15:48:15 +0100

    App Reparties - external project - adding chat client (socket with JavaFX)

---
+++ Semester2/AppReparties/External/ChatSocket/ChatClient/src/main/java/com/houarizegai/chatfx/App.java
@@ -0,0 +1,27 @@
+
+package com.houarizegai.chatfx;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Client.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Client");
+        stage.getIcons().add(new Image("/images/client-48px.png"));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
