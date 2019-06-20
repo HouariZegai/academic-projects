9f9dffff4fea6a84f7f514c5ee9665a5b315d932
HouariZegai <houarizegai14@gmail.com>
2019-06-20 15:47:57 +0100

    App Reparties - external project - adding chat server (socket with JavaFX)

---
+++ Semester2/AppReparties/External/ChatSocket/ChatServer/src/main/java/com/houarizegai/chatfx/controllers/ServerController.java
@@ -0,0 +1,130 @@
+package com.houarizegai.chatfx.controllers;
+
+import com.houarizegai.chatfx.global.Config;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextField;
+
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ServerController implements Initializable {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private JFXListView<HBox> msgNodes;
+
+    @FXML
+    private JFXTextField msgField;
+
+    /* Start Msg variables */
+
+    public static ServerSocket serverSocket;
+    public static Socket socket;
+    public static DataInputStream dataInputStream;
+    public static DataOutputStream dataOutputStream;
+
+    /* End Msg variables */
+
+    public static boolean killServer = false;
+    public String msgInput;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        root.setOnKeyPressed(e -> {
+            if (e.getCode().equals(KeyCode.ENTER)) {
+                sendMsg();
+            }
+        });
+
+        (new Thread() {
+            @Override
+            public void run() {
+
+                try {
+                    serverSocket = new ServerSocket(Config.port);
+                    socket = serverSocket.accept();
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                    msgInput = "";
+                    while (!msgInput.equals("exit")) {
+                        msgInput = dataInputStream.readUTF();
+
+                        Platform.runLater(() -> addMsg(false));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @FXML
+    private void sendMsg() {
+        if (msgField.getText() == null || msgField.getText().trim().isEmpty())
+            return;
+
+        try {
+            dataOutputStream.writeUTF(msgField.getText().trim());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        msgInput = msgField.getText().trim();
+        addMsg(true);
+        msgField.setText(null);
+
+    }
+
+    @FXML
+    private void emojiChooser() {
+
+    }
+
+    private void addMsg(boolean senderIsServer) {
+        Label lbl = new Label(msgInput);
+        lbl.setStyle("-fx-font-size: 16px;"
+                + "-fx-background-color: #" + (senderIsServer ? "B00020" : "2196f3") + ";"
+                + "-fx-text-fill: #FFF;"
+                + "-fx-background-radius:25;"
+                + "-fx-padding: 10px;");
+        lbl.setWrapText(true);
+        lbl.setMaxWidth(400);
+
+        HBox container = new HBox();
+        if (!senderIsServer) {
+            container.getChildren().add(new ImageView(new Image("/images/client-48px.png")));
+            container.setAlignment(Pos.CENTER_LEFT);
+            container.setSpacing(10);
+            container.setPadding(new Insets(0, 10, 0, 0));
+        } else {
+            container.setAlignment(Pos.CENTER_RIGHT);
+            container.setPadding(new Insets(0, 10, 0, 10));
+        }
+        container.getChildren().add(lbl);
+        container.setPrefHeight(40);
+
+        msgNodes.getItems().add(container);
+    }
+
+}
---
+++ Semester2/AppReparties/External/ChatSocket/ChatServer/src/main/java/com/houarizegai/chatfx/global/Config.java
@@ -0,0 +1,5 @@
+package com.houarizegai.chatfx.global;
+
+public class Config {
+    public static int port;
+}
