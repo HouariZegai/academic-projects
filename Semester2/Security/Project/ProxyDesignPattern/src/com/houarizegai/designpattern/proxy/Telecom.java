d2b30878924100031891af255ba898bbe91657f4
HouariZegai <houarizegai14@gmail.com>
2019-07-06 11:11:54 +0100

    Security - Project (Proxy design pattern) - remove speed attribute from telecom

--- Semester2/Security/Project/ProxyDesignPattern/src/com/houarizegai/designpattern/proxy/Telecom.java
+++ Semester2/Security/Project/ProxyDesignPattern/src/com/houarizegai/designpattern/proxy/Telecom.java
@@ -2,18 +2,8 @@ package com.houarizegai.designpattern.proxy;
 
 public class Telecom implements ISP {
 
-    private int browsingSpeed = 10;
-
     @Override
     public String serverSite(String url) {
         return String.format("https://%s.com", url);
-    }
-
-    public int getBrowsingSpeed() {
-        return browsingSpeed;
-    }
-
-    public void setBrowsingSpeed(int browsingSpeed) {
-        this.browsingSpeed = browsingSpeed;
     }
 }
